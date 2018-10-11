79fc948d2fe922fdd71236fba1dbf352d0366cc0
Julio Betta <juliobetta@gmail.com>
2018-10-11 17:55:00 -0300

    add dotenv and apply new  mongoose version settings

--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
+const path = require('path');
+const result = require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+if (result.error) {
+  throw result.error;
+}
+
 const express = require('express');
 const models = require('./models');
 const expressGraphQL = require('express-graphql');
@@ -7,14 +14,13 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-// Replace with your mongoLab URI
-const MONGO_URI = '';
-if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
-}
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_LAB_URI } = process.env;
+const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_LAB_URI}`;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+  useMongoClient: true
+});
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
